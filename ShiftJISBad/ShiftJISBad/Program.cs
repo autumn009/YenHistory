d1df0f552f606ddc35af99da6394bd6bae08bf84
Akira Kawamata <autumn@piedey.co.jp>
2023-08-21 11:24:14 +0900

    add ShiftJIS

---
+++ ShiftJISBad/ShiftJISBad/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+const string target = @"c:\ビジネス文書\企画表.txt";
+byte[] bytes = Encoding.GetEncoding("Shift_JIS").GetBytes(target);
+int count  = 0;
+for (int i = 0; i < bytes.Length; i++)
+{
+    if(bytes[i] == '\\') count++;
+}
+Console.WriteLine($"{target}のディレクトリ区切り記号の数は{count}個です。");
---
+++ ShiftJISGood/ShiftJISGood/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+const string target = @"c:\ビジネス文書\企画表.txt";
+byte[] bytes = Encoding.GetEncoding("Shift_JIS").GetBytes(target);
+int count = 0;
+int pointer = 0;
+for (; ; )
+{
+    if (IsDBCSLeadByte(bytes[pointer]))
+        pointer += 2;
+    else
+    {
+        if (bytes[pointer] == '\\') count++;
+        pointer += 1;
+    }
+    if (pointer >= bytes.Length) break;
+}
+Console.WriteLine($"{target}のディレクトリ区切り記号の数は{count}個です。");
+
+bool IsDBCSLeadByte(byte ch)
+{
+    return (ch >= 0x81 && ch <= 0x9f) || (ch >= 0xe0 && ch <= 0xef);
+}
