72255fcc0d8b2a680458c23fa9dd1191241ee7cc
Akira Kawamata <autumn@piedey.co.jp>
2023-08-22 19:29:03 +0900

    add JpCn

---
+++ JpCn/JpCn/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Globalization;
+using var writer = File.CreateText("target.txt");
+CultureInfo.CurrentCulture = new CultureInfo("ja-JP", false);
+writer.WriteLine(123.ToString("C"));
+CultureInfo.CurrentCulture = new CultureInfo("zh-CN", false);
+writer.WriteLine(123.ToString("C"));
